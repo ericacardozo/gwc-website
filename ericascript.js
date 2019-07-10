1c93abd2e0126ed18ee8a6e123397719e2f1f4ec
ericacardozo <52722831+ericacardozo@users.noreply.github.com>
2019-07-10 15:26:29 -0700

    Add files via upload

---
+++ ericascript.js
@@ -0,0 +1,7 @@
+var myimgobj = document.images["jsbutton"];
+
+function changeImage()
+{
+document.images["jsbutton"].src= "https://hulahunnie.files.wordpress.com/2016/08/contact-me-button.png";
+return true;
+}
