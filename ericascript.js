1608af1a0c3931fff431c066098774e6de86753d
ericacardozo <52722831+ericacardozo@users.noreply.github.com>
2019-07-10 15:27:34 -0700

    Delete ericascript.js

--- ericascript.js
+++
@@ -1,7 +0,0 @@
-var myimgobj = document.images["jsbutton"];
-
-function changeImage()
-{
-document.images["jsbutton"].src= "https://hulahunnie.files.wordpress.com/2016/08/contact-me-button.png";
-return true;
-}
